6df00bf03306174f968b1139a6a77d870fc89f49
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-30 23:23:47 -0300

    feat(database): add SQLite drivers
    Add `sqlite3` and `sqlite` drivers fro connection with local database.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
-import { routes } from './routes.ts';
-import { sequelize } from './db/db.ts';
+import routes from './routes.ts';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes)
 
 export async function bootstrap() {
-  await sequelize.sync();
   app.listen(3000);
 }
